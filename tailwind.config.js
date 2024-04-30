6528d5a03dcc4c1650cb10b16d6d57be0c5f95f7
mudit088 <muditsri25@gmail.com>
2024-04-30 22:43:46 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: [  "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      transform: {
+        'skew-10-20': 'skew(10deg, 20deg)', // Define custom skew transform
+      },
+    },
     fontFamily :{
       Vsoc :["Protest Revolution","sans-serif"],
     },
@@ -11,3 +15,12 @@ export default {
   plugins: [],
 }
 
+// module.exports = {
+//   theme: {
+//     extend: {
+//       transform: {
+//         'skew-10-20': 'skew(10deg, 20deg)', // Define custom skew transform
+//       },
+//     },
+//   },
+// }
