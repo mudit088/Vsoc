529039b4213ba797fe0ed01deec3458dead62899
mudit088 <muditsri25@gmail.com>
2024-05-11 21:13:18 +0530

    updated

--- src/Components/Front.jsx
+++ src/Components/Front.jsx
@@ -101,7 +101,7 @@ const Front = () => {
               </div>
               {/* Devfolio Apply Button */}
               <div
-                className="mb-36"
+                className="absolute"
                 data-hackathon-slug="YOUR-HACKATHON-SLUG"
                 data-button-theme="light"
                 style={{ height: '44px', width: '312px' }}
